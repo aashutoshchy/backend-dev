fce9657cac13366c7da3ffded50b08b34c324635
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-06-03 17:51:12 +0545

    Add folders

---
+++ 02-nodejs-modules/main.js
@@ -0,0 +1,23 @@
+// import { createServer } from 'node:http';
+
+// import { a } from "./mymodules.js"
+import obj from './mymodules.js';
+
+// // console.log(a)
+console.log(obj)
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// const a = require("./mymodule2.js");
+// console.log(a);
---
+++ 02-nodejs-modules/mymodules.js
@@ -0,0 +1,9 @@
+export const a = 1;
+export const b = 2;
+
+const obj = {
+    x: 1,
+    y: 2
+};
+
+export default obj;
